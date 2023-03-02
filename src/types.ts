7777c8b08e5bc04a87e8c7a44cd4a7bf1a434f53
Rune Botten <rbotten@gmail.com>
2023-03-02 13:00:19 -0800

    fix: remove @sanity/client as a dependency

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,21 @@
-import { SanityDocumentStub } from '@sanity/client'
-
 export type AlgoliaRecord = Readonly<Record<string, any>>
 
+export type DocumentStub = Record<string, any> & {
+  _id: string
+  _type: string
+  _rev: string
+}
+
+export interface SanityApiclient {
+  fetch: (query: string, params?: any) => Promise<DocumentStub[]>
+}
+
 export interface SerializeFunction {
-  (document: SanityDocumentStub): AlgoliaRecord
+  (document: DocumentStub): AlgoliaRecord
 }
 
 export interface VisiblityFunction {
-  (document: SanityDocumentStub): boolean
+  (document: DocumentStub): boolean
 }
 
 export type WebhookBody = {
